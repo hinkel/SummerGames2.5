0bd8097b14d85f8168b7f0e2461411379fc7da0f
hinkel <hinkel@users.noreply.github.com>
2014-04-19 15:44:10 +0200

    Update config.c

--- src/config.c
+++ src/config.c
@@ -185,7 +185,7 @@ static void resetConf(void)
     cfg.accz_alt_cf               = 0.940f;     // Crashpilot: Value for complementary filter accz and altitude
     cfg.baro_lag                  = 0.3f;       // Lag of Baro/Althold stuff in general, makes stop in hightchange snappier
     cfg.barodownscale             = 0.7f;       // Scale downmovement down (because copter drops faster than rising)
-    cfg.al_suptime                = 2000;       // 0 = disable // althold is engage autonomously after for ex: 2000 ms when throttle stick is not moving deadband fix = 25 Throttle
+    cfg.al_suptime                = 2000;       // 0 = disable // althold is engage autonomously after for ex: 2000 ms when throttle stick is not moving deadband fix = 30 Throttle
   
     // Autoland
     cfg.al_barolr                 = 110;         // Temporary value "64" increase to increase Landingspeed
