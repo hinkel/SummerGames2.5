8777b96350850604bec71b7c965d8236517dd71b
hinkel <hinkel@users.noreply.github.com>
2014-12-31 12:21:05 +0100

    Update cli.c
    SPIDER6X

--- src/cli.c
+++ src/cli.c
@@ -80,7 +80,7 @@ const char * const mixerNames[] =
 {
     "TRI", "QUADP", "QUADX", "BI",
     "GIMBAL", "Y6", "HEX6",
-    "FLYING_WING", "Y4", "HEX6X", "OCTOX8", "OCTOFLATP", "OCTOFLATX",
+    "FLYING_WING", "Y4", "SPIDER6X", "OCTOX8", "OCTOFLATP", "OCTOFLATX",
     "AIRPLANE", "HELI_120_CCPM", "HELI_90_DEG", "VTAIL4", "CUSTOM", NULL
 };
 
